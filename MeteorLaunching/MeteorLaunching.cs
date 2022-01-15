3c8ad7a995eca987fb8b201d91304bd32effba38
12090113 <47487266+12090113@users.noreply.github.com>
2022-01-14 18:16:36 -0800

    Added list of projectiles that can be cycled through

--- MeteorLaunching/MeteorLaunching.cs
+++ MeteorLaunching/MeteorLaunching.cs
@@ -10,7 +10,8 @@ namespace MeteorLaunching
     {
         PlayerBody playerBody;
         Transform launcher;
-        GameObject meteor;
+        GameObject[] projectiles;
+        int p = 0;
         SimpleFluidVolume sunFluid;
         protected OWAudioSource audio;
         public float launchSpeed = 1000;
@@ -23,7 +24,10 @@ namespace MeteorLaunching
             {
                 if (loadScene != OWScene.SolarSystem) return;
                 playerBody = FindObjectOfType<PlayerBody>();
-                meteor = GameObject.Find("Player_Body/RoastingSystem").GetComponent<RoastingStickController>()._mallowBodyPrefab; //GameObject.Find("VolcanicMoon_Body/Sector_VM/Effects_VM/VolcanoPivot/MeteorLauncher").GetComponent<MeteorLauncher>()._meteorPrefab;
+                projectiles = new GameObject[] {
+                    GameObject.Find("VolcanicMoon_Body/Sector_VM/Effects_VM/VolcanoPivot/MeteorLauncher").GetComponent<MeteorLauncher>()._meteorPrefab,
+                    GameObject.Find("Player_Body/RoastingSystem").GetComponent<RoastingStickController>()._mallowBodyPrefab
+                };
                 sunFluid = GameObject.Find("Sun_Body/Sector_SUN/Volumes_SUN/ScaledVolumesRoot/DestructionFluidVolume").GetComponent<SimpleFluidVolume>();
                 audio = GameObject.Find("Player_Body/Audio_Player/OneShotAudio_Player").GetComponent<OWAudioSource>();
                 StartCoroutine(SetLauncher());
@@ -40,7 +44,7 @@ namespace MeteorLaunching
         {
             if (BackPressed() || Mouse.current.middleButton.wasPressedThisFrame)
             {
-                GameObject newMeteor = Instantiate(meteor, launcher.position + launcher.forward * launchSize * meteor.GetComponentInChildren<MeshRenderer>().bounds.size.x, launcher.rotation);
+                GameObject newMeteor = Instantiate(projectiles[p], launcher.position + launcher.forward * .5f + launcher.forward * launchSize * projectiles[p].GetComponentInChildren<MeshRenderer>().bounds.size.x * .5f, launcher.rotation);
                 /*GameObject newMeteor = GameObject.CreatePrimitive(PrimitiveType.Cube);
                 newMeteor.transform.position = launcher.position + launcher.forward * launchSize * 2;
                 newMeteor.transform.rotation = launcher.rotation;
@@ -52,31 +56,41 @@ namespace MeteorLaunching
                 var fluid = Detector.AddComponent<DynamicFluidDetector>();
                 owrigid._attachedForceDetector = force;
                 owrigid._attachedFluidDetector = fluid;*/
-                Destroy(newMeteor.GetComponent<SelfDestruct>());
-                newMeteor.GetComponentInChildren<MeshRenderer>().material.color = new Color(1, 1, 1, 0);
-                
                 newMeteor.GetComponent<Rigidbody>().velocity = launcher.forward * launchSpeed;
                 newMeteor.transform.localScale = new Vector3(launchSize, launchSize, launchSize);
-                newMeteor.name = "pew pew KABOOM";
-                /*
-                FluidVolume closeFluid = sunFluid;
-                var fluids = FindObjectsOfType<SimpleFluidVolume>();
-                foreach (var fluid in fluids)
+                newMeteor.name = "Projectile";
+                if (p == 1)
                 {
-                    float distance = Vector3.Distance(playerBody.GetPosition(), fluid.transform.position);
-                    if (distance < 1000 && fluid._allowShipAutoroll && distance < Vector3.Distance(playerBody.GetPosition(), closeFluid.transform.position))
-                    {
-                        closeFluid = fluid;
-                    }
+                    Destroy(newMeteor.GetComponent<SelfDestruct>());
+                    newMeteor.GetComponentInChildren<MeshRenderer>().material.color = new Color(1, 1, 1, 0);
                 }
-                //ModHelper.Console.WriteLine($"" + closeFluid);
                 
-                newMeteor.transform.Find("ConstantDetectors").GetComponent<ConstantFluidDetector>()._onlyDetectableFluid = closeFluid;
-                MeteorController newMeteorContr = newMeteor.GetComponent<MeteorController>();
-                newMeteorContr._heat = 1;
-                newMeteorContr._hasLaunched = true;
-                newMeteorContr._suspendRoot = playerBody.transform;*/
+                else if (p == 0) {
+                    FluidVolume closeFluid = sunFluid;
+                    var fluids = FindObjectsOfType<SimpleFluidVolume>();
+                    foreach (var fluid in fluids)
+                    {
+                        float distance = Vector3.Distance(playerBody.GetPosition(), fluid.transform.position);
+                        if (distance < 1000 && fluid._allowShipAutoroll && distance < Vector3.Distance(playerBody.GetPosition(), closeFluid.transform.position))
+                        {
+                            closeFluid = fluid;
+                        }
+                    }
+                    //ModHelper.Console.WriteLine($"" + closeFluid);
+                    newMeteor.transform.Find("ConstantDetectors").GetComponent<ConstantFluidDetector>()._onlyDetectableFluid = closeFluid;
+                    MeteorController newMeteorContr = newMeteor.GetComponent<MeteorController>();
+                    newMeteorContr._heat = 1;
+                    newMeteorContr._hasLaunched = true;
+                    newMeteorContr._suspendRoot = playerBody.transform;
+                }
                 audio.PlayOneShot(AudioType.BH_MeteorLaunch, 0.25f);
+            }
+            if (Keyboard.current.lKey.wasPressedThisFrame || (OWInput.IsNewlyPressed(InputLibrary.rollMode) && BackPressed()))
+            {
+                p++;
+                if (p > projectiles.Length)
+                    p = 0;
+                ModHelper.Console.WriteLine($"" + projectiles[p]);
             }
         }
 
